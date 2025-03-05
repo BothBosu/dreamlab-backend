9948f77b0a0d3579a53ddb1de7be54015ceac543
Pitipat Gumphusiri <pitipat.both@gmail.com>
2025-03-05 10:20:55 +0700

    add UserRepository

---
+++ src/main/java/com/muic/ssc/backend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.muic.ssc.backend.repository;
+
+import com.muic.ssc.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
