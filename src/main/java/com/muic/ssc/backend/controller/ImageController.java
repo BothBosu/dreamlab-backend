ea22dad28833c7e2328b2ae551c3238425120113
szuchihsu <szuchi.hsu@student.mahidol.edu>
2025-03-13 14:09:15 +0700

    Added image controller

---
+++ src/main/java/com/muic/ssc/backend/controller/ImageController.java
@@ -0,0 +1,43 @@
+package com.muic.ssc.backend.controller;
+
+import com.muic.ssc.backend.entity.Image;
+import com.muic.ssc.backend.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImageController {
+
+    @Autowired
+    private ImageService imageService;
+
+    @PostMapping("/upload")
+    public ResponseEntity<Image> uploadImage(@RequestParam("file") MultipartFile file) {
+        Image image = imageService.save(file);
+        return ResponseEntity.ok(image);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Image>> getAllImages() {
+        List<Image> images = imageService.getAllImages();
+        return ResponseEntity.ok(images);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Image> getImageById(@PathVariable Long id) {
+        return imageService.getImageById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteImage(@PathVariable Long id) {
+        imageService.deleteImage(id);
+        return ResponseEntity.noContent().build();
+    }
+}
