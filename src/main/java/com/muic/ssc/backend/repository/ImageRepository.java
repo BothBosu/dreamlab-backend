051362eee5cb7ae2d61c843906b9c267bc6d1a3b
szuchihsu <szuchi.hsu@student.mahidol.edu>
2025-03-13 14:09:26 +0700

    Added image repository

---
+++ src/main/java/com/muic/ssc/backend/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.muic.ssc.backend.repository;
+
+import com.muic.ssc.backend.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageRepository extends JpaRepository<Image, Long> {
+}
