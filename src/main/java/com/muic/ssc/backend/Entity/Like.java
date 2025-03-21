d3ad690fded945883a721562c03f2268483fe5df
szuchihsu <szuchi.hsu@student.mahidol.edu>
2025-03-21 15:28:04 +0700

    Added Like entity

---
+++ src/main/java/com/muic/ssc/backend/Entity/Like.java
@@ -0,0 +1,38 @@
+package com.muic.ssc.backend.Entity;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "likes")
+public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    @ManyToOne(optional = false)
+    private Image image;
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
