7a4dde7ca10901c32d07395793d0c2a0f02f2449
szuchihsu <szuchi.hsu@student.mahidol.edu>
2025-03-21 11:22:58 +0700

    Added api/images to security filter

--- src/main/java/com/muic/ssc/backend/Config/SecurityConfig.java
+++ src/main/java/com/muic/ssc/backend/Config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
