a719347c0adb6b4c75e48255fb210954bb0ec5a1
szuchihsu <szuchi.hsu@student.mahidol.edu>
2025-03-30 23:21:50 +0700

    Added method to delete likes by image id

--- src/main/java/com/muic/ssc/backend/Repository/LikeRepository.java
+++ src/main/java/com/muic/ssc/backend/Repository/LikeRepository.java
@@ -4,11 +4,18 @@ import com.muic.ssc.backend.Entity.Image;
 import com.muic.ssc.backend.Entity.Like;
 import com.muic.ssc.backend.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     boolean existsByUserAndImage(User user, Image image);
     int countByImage(Image image);
     void deleteByUserAndImage(User user, Image image);
-}
 
+    @Modifying
+    @Query("DELETE FROM Like l WHERE l.image.id = :imageId")
+    void deleteByImageId(@Param("imageId") Long imageId);
+}
